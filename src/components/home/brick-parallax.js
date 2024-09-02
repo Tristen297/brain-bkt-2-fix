df6caf99b4a538522a95b5a4c715fea22498a129
Tristen Dean <103838311+Tristen297@users.noreply.github.com>
2024-09-02 01:57:13 -0400

    still fixing

--- src/components/home/brick-parallax.js
+++ src/components/home/brick-parallax.js
@@ -3,9 +3,7 @@ import "../../styles/home/brick-parallax.css";
 function BrickParallax() {
     return(
         <div className="parallax-content">
-            <a href="">
 
-            </a>
         </div>
     )
 }
